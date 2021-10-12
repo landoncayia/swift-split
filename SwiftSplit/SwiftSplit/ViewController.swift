51ccbdd01e5ddb7bb0234872175dd5b4d4122299
Austin Block <45405990+austin12block@users.noreply.github.com>
2021-10-12 10:46:26 -0400

    Created xcode project

---
+++ SwiftSplit/SwiftSplit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftSplit
+//
+//  Created by Austin Block on 10/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
