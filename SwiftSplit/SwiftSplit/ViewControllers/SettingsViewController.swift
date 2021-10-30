ebab243478693d1746f8e42091abf0c21a39d6e7
Landon Cayia <lcayia@uvm.edu>
2021-10-30 17:19:10 -0400

    Settings UITable implemented; needs actual settings now

--- SwiftSplit/SwiftSplit/ViewControllers/SettingsViewController.swift
+++ SwiftSplit/SwiftSplit/ViewControllers/SettingsViewController.swift
@@ -7,7 +7,29 @@
 
 import UIKit
 
-class SettingsViewController : UIViewController {
+class SettingsCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+}
+
+class SettingsViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var settingsTable: UITableView! {
+        didSet {
+            settingsTable.delegate = self;
+            settingsTable.dataSource = self;
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = settingsTable.dequeueReusableCell(withIdentifier: "SettingsCell") as! SettingsCell
+        cell.nameLabel.text = "Setting"
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
