b98f4d50039ecbff3153d402536d7c5e0c256b00
Austin Block <45405990+austin12block@users.noreply.github.com>
2021-10-30 16:20:35 -0400

    Budgets actually kinda work, its RAD

--- SwiftSplit/SwiftSplit/Budget/BudgetViewController.swift
+++ SwiftSplit/SwiftSplit/Budget/BudgetViewController.swift
@@ -13,7 +13,7 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
     var receiptStore: ReceiptStore!
     
     // the list of people and amounts to show
-    var budgetStore = [BudgetItem]()
+    var allBudgets = [BudgetItem]()
     
     // Date picker widget outlets (to modify from Swift)
     @IBOutlet var fromDatePicker: UIDatePicker!
@@ -39,8 +39,8 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("budgetStore count = ", budgetStore.count)
-        return budgetStore.count
+        print("allBudgets count = ", allBudgets.count)
+        return allBudgets.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -48,12 +48,20 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
         // This is the cell
         let cell = budgetTable.dequeueReusableCell(withIdentifier: "BudgetCell") as! BudgetCell
         
+        // Swift is dumb
+        cell.preservesSuperviewLayoutMargins = false
+        cell.separatorInset = UIEdgeInsets.zero
+        cell.layoutMargins = UIEdgeInsets.zero
+        
         // This is the item
-        let item = budgetStore[indexPath.row]
+        let item = allBudgets[indexPath.row]
         
         // Set the cells contents
         cell.nameLabel.text = item.person.name
-        cell.dollarLabel.text = String(item.amount)
+        
+        if let displayAmt = numberFormatter.string(from: NSNumber(value: item.amount)) {
+            cell.dollarLabel.text = "$" + displayAmt
+        }
         
         return cell
     }
@@ -79,7 +87,7 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
         print("processReceipts")
         
         // Clear out the list of receipts
-        budgetStore.removeAll()
+        allBudgets.removeAll()
         
         // Get the range we need
         let minDate = fromDatePicker.date
@@ -90,15 +98,26 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
             
             // Is this receipt within the range?
             if isBetween(from: minDate, to: maxDate, toCheck: receiptDate) {
-                // TODO: This is setup only for testing
                 
-                // Need to add each perons
+                // Get the receipt totals, then we'll break them up
                 let theseTotals = receipt.getTotals()
                 
-                // Create a budget item
-                let newItem = BudgetItem(person: theseTotals.person, amount: theseTotals.amount)
+                for total in theseTotals {
+                    let currPerson = total.person
+                    let currAmount = total.amount
+                    
+                    if let index = allBudgets.firstIndex(where: {$0.person == currPerson}) {
+                        // There is already a budget entry for this person, so add to it
+                        let addAmt = allBudgets[index].amount
+                        allBudgets[index].amount = currAmount + addAmt
+                    } else {
+                        // There was not already a budget entry for this person
+                        let newItem = BudgetItem(person: total.person, amount: total.amount)
+                        allBudgets.append(newItem)
+                    }
+
+                }
                 
-                budgetStore.append(newItem)
             }
             
         }
@@ -110,6 +129,17 @@ class BudgetViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func isBetween(from: Date, to: Date, toCheck: Date) -> Bool {
+        // TODO: There is a bug here
+        // It includes time in the query, not just the date
+        // So it doesnt get receipts to the END of the day
         return (min(from, to) ... max(from, to)).contains(toCheck)
     }
+    
+    let numberFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }()
 }
