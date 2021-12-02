6a181090ab0e018184c1e18beb339f1c261b0051
Mike <mferro@uvm.edu>
2021-12-02 16:08:47 -0500

    Starting to add validation for Next button on ReceiptViewController Added UserTotalCell Added ReceiptTotalsVIewController need to connect things in both of these

--- SwiftSplit/SwiftSplit/Receipts/ReceiptTotalsViewController.swift
+++ SwiftSplit/SwiftSplit/Receipts/ReceiptTotalsViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
-class ReceiptTotalsViewController: UIViewController/*, UITableViewDataSource, UITableViewDelegate*/ {
+class ReceiptTotalsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var receipt:  Receipt!
+    var totals = [ReceiptTotal]()
+    
+    // TODO: connect
     @IBOutlet var tax: UILabel!
     @IBOutlet var total: UILabel!
-    /*
+    
     @IBOutlet var userTableView: UITableView! {
         didSet {
             userTableView.delegate = self
@@ -19,13 +22,21 @@ class ReceiptTotalsViewController: UIViewController/*, UITableViewDataSource, UI
             userTableView.rowHeight = UITableView.automaticDimension
         }
     }
+
+    override func viewDidLoad() {
+        totals = receipt.getTotals()
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return totals.count
+    }
         
+    // TODO: change default tablecells to UserTotalCells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = userTableView.dequeueReusableCell(withIdentifier: "UserTotalCell", for: indexPath) as! UserTotalCell
+        cell.name.text = totals[indexPath.row].person.name
+        cell.total.text = "$"+String(totals[indexPath.row].amount)
+        return cell
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-     */
 }
