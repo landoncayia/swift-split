c745e796b2c53b9e8533a0f1c3007f935e4e3251
Austin Block <45405990+austin12block@users.noreply.github.com>
2021-12-02 17:19:22 -0500

    Logic for tab clicked

--- SwiftSplit/SwiftSplit/ViewControllers/TabViewController.swift
+++ SwiftSplit/SwiftSplit/ViewControllers/TabViewController.swift
@@ -1,10 +1,26 @@
 // TabViewController
 
 import UIKit
-class TabViewController : UITabBarController{
+class TabViewController : UITabBarController, UITabBarControllerDelegate {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
     }
+
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        print("tab bar clicked for: ", viewController)
+        if viewController is CreateViewController {
+            print("Create tab clicked")
+        } else if viewController is BudgetViewController {
+            print("Budget tab clicked")
+        } else if viewController is SettingsViewController {
+            print("Settings tab clicked")
+        } else {
+            print("Unknown tab clicked")
+        }
+    }
+
 }
 
