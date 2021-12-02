dab26aef501d0e61a60e1ca56e36a4338159c818
Austin Block <45405990+austin12block@users.noreply.github.com>
2021-12-02 17:24:20 -0500

    Better logic for tab bar clicks

--- SwiftSplit/SwiftSplit/ViewControllers/TabViewController.swift
+++ SwiftSplit/SwiftSplit/ViewControllers/TabViewController.swift
@@ -10,15 +10,10 @@ class TabViewController : UITabBarController, UITabBarControllerDelegate {
     }
 
     func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-        print("tab bar clicked for: ", viewController)
-        if viewController is CreateViewController {
+        let id = viewController.restorationIdentifier ?? ""
+        print("tab bar clicked for: ", id)
+        if id == "CreateNavViewController" {
             print("Create tab clicked")
-        } else if viewController is BudgetViewController {
-            print("Budget tab clicked")
-        } else if viewController is SettingsViewController {
-            print("Settings tab clicked")
-        } else {
-            print("Unknown tab clicked")
         }
     }
 
