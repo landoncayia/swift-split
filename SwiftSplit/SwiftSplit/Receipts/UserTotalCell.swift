d1772f009970e84badde94889564373db28448c3
Austin Block <45405990+austin12block@users.noreply.github.com>
2021-12-04 15:54:42 -0500

    Constraints and accessibility on Create tab

--- SwiftSplit/SwiftSplit/Receipts/UserTotalCell.swift
+++ SwiftSplit/SwiftSplit/Receipts/UserTotalCell.swift
@@ -10,4 +10,20 @@ class UserTotalCell: UITableViewCell {
     //TODO: connect
     @IBOutlet var name: UILabel!
     @IBOutlet var total: UILabel!
+    
+    override func layoutSubviews() {
+        
+        super.layoutSubviews()
+        
+        // Make the cell less 'selectable'
+        self.selectionStyle = .none
+        
+        // Make the cell pretty
+        let verticalPadding: CGFloat = 10
+        let maskLayer = CALayer()
+        maskLayer.cornerRadius = 8    //if you want round edges
+        maskLayer.backgroundColor = UIColor.black.cgColor
+        maskLayer.frame = CGRect(x: self.bounds.origin.x, y: self.bounds.origin.y, width: self.bounds.width, height: self.bounds.height).insetBy(dx: 0, dy: verticalPadding/2)
+        self.layer.mask = maskLayer
+    }
 }
